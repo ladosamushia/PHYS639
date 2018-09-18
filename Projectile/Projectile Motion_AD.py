82b998eba68189926ae01decf55d31b7e26c46e2
atdortch <atdortch@ksu.edu>
2018-09-17 23:42:40 -0500

    Add files via upload

--- Projectile/Projectile Motion_AD.py
+++ Projectile/Projectile Motion_AD.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 x0 = 0
 y0 = 0
 y = 20
-v0x = 100
-v0y = 100
+v0x = 500
+v0y = 500
 tini = 0
-dt = 0.9999
+dt = .01
 
 def displacement(x0,y0,v0x,v0y,ax,ay,dt): #subset of equations to find delta(x)
     
@@ -29,14 +29,12 @@ x = x0
 y = y0
 Vx = v0x
 Vy = v0y
-while y>0: #calculate displacement while y is greater than zero.
+while y>=0: #calculate displacement while y is greater than zero.
    
     ay = -0.00004*(Vx**2+Vy**2)-9.8
     ax =  -0.00004*(Vx**2+Vy**2)
     [x,y,Vx,Vy] = displacement(x,y,Vx,Vy,ax,ay,dt)
-    if y>0:
-        print(y
-        plt.plot(x, y,'.g')
+    plt.plot(x,y,'.g')
     
     
     
@@ -64,9 +62,46 @@ ax=0
 ay=-9.8
 while y>=0: #calculate displacement while y is greater than zero.
     [x,y,Vx,Vy] = displacement(x,y,Vx,Vy,ax,ay,dt)
-    plt.plot(x,y,'.r')
- 
+    plt.hold(True)
+    plt.plot(x,y,'.b')
+    
+    
 
+
+x0 = 0
+y0 = 0
+y = 20
+tini = 0
+ax = 0
+
+def displacement(x0,y0,v0x,v0y,ax,ay,dt): #subset of equations to find delta(x)
+    
+    x = x0 + v0x*dt
+    y = y0 + v0y*dt 
+    Vx = v0x + ax*dt
+    Vy = v0y + ay*dt
+    return([x,y,Vx,Vy])
+    
+x = x0
+y = y0
+Vx = v0x
+Vy = v0y
+while y>=0: #calculate displacement while y is greater than zero.
+    ay = -0.00004*(Vx**2+Vy**2)*(1-2.2e-5*y)**(5/2) - 9.8
+    ax =  -0.00004*(Vx**2+Vy**2)*(1-2.2e-5*y)**(5/2)
+    [x,y,Vx,Vy] = displacement(x,y,Vx,Vy,ax,ay,dt)
+    plt.hold(True)
+    plt.plot(x,y,'.r')
+    
+
+
+
+    
+    
+    
+    
+    
+    
     
     
     
