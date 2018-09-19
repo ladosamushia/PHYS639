88c53a65e51f202c00da39bd6169c8897502178f
ladosamushia <colkhis@gmail.com>
2018-09-19 11:53:54 -0500

    Update Warmup_milo.py

--- Warmup/Warmup_milo.py
+++ Warmup/Warmup_milo.py
@@ -7,20 +7,24 @@ Created on Sun Sep 16 00:10:56 2018
 
 import numpy as np 
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 #initialize
 N = 1000 # DEFINE number of particles
-tau = 7.038*(10**8) # halflife
+tau = 1000 # halflife
 t0 = 0 # DEFINE start time
 tfin = 1000 # DEFINE final time
 dt = (tfin - t0)/1000 # stepsize
 
 num = np.array([]) # make storage array for number N at time t
+time = np.array([])
 
 #loop t from t0 to tfin
-for t in range(t0, tfin, 1000):
+for t in range(t0, tfin):
     DNDT = -N/tau
     N = N + (DNDT*dt)
-    num = np.append(N, t)
-
-plt.plot(t,num[t])
+    num = np.append(num, N)
+    time = np.append(time, t)
+    #print(t,N,tau,DNDT,dt)
+plt.plot(time,num,'.')
+plt.plot(time,1000*np.exp(-time/tau),'r')
