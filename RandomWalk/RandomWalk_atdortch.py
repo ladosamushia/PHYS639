737ba823d79e3e8392ecc5ba997209f1fc36e6e3
atdortch <atdortch@ksu.edu>
2018-09-29 13:22:23 -0500

    Add files via upload

--- RandomWalk/RandomWalk_atdortch.py
+++ RandomWalk/RandomWalk_atdortch.py
@@ -11,26 +11,110 @@ import matplotlib.pyplot as plt
 
 
 
-particles = 10
-steps = 20
+particles = 100
+steps = np.arange(1000)
+Nsteps = 1000
 
 
 
-
-position = np.zeros(particles)      # position of particles start at zero
+position = np.zeros(Nsteps)      # position of particles start at zero
 
 right = 1; left = 2                 # assign values to left and right positions
 
-for step in range(steps):           
+for step in steps:           
     for p in range(particles):
         walk = rand.randint(1,2)
         if walk == right:
-            position[p] += 1
+            position[step] += 1
         elif walk == left:
-            position[p] -=1
+            position[step] -=1
 displacement = position-0
 
 print(position)
 
-plt.plot(steps,displacement)
+plt.plot(steps,displacement,'og')
+
+
+
+#----------------------------------------------------------------------
+
+import numpy as np
+import random as rand
+import matplotlib.pyplot as plt
+
+
+
+particles = 100
+steps = np.arange(1000)
+Nsteps = 1000
+
+
+
+position = np.zeros(Nsteps)      # position of particles start at zero
+
+right = 1; left = 2                 # assign values to left and right positions
+
+for step in steps:           
+    for p in range(particles):
+        walk = rand.randint(1,2)
+        if walk == right:
+            position[step] += 1
+        elif walk == left:
+            position[step] -=3
+displacement = position-0
+
+print(position)
+
+plt.plot(steps,displacement,'og')
+
+
+#-------------------------------------------------------------------------
+
+import numpy as np
+import random as rand
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+ax = plt.axes(projection='3d')
+fig = plt.figure()
+ax.scatter3D(steps,displacement)
+x = steps
+y = displacement
+
+particles = 100
+steps = np.arange(1000)
+Nsteps = 1000
+
+
+
+position = np.zeros(Nsteps)      # position of particles start at zero
+
+right = 1; left = 2; forward = 3; backward = 4; up = 5; down = 6
+
+for step in steps:           
+    for p in range(particles):
+        walk = rand.randint(1,7)
+        if walk == right:
+            position[step] += 1
+        elif walk == left:
+            position[step] -=1
+        elif walk == forward:
+            position[step] +=1
+        elif walk == backward:
+            position[step] -=1
+        elif walk == up:
+            position[step] +=1
+        elif walk == down:
+            position[step] -=1
+displacement = position-0
+
+z = np.linspace(up,down)
+print(position)
+
+
+
+ax.scatter3D(x,y,z,'g')
+
+
 
