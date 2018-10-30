b8a405df35b73a23a3051fb92bf6562e02fc2c35
Jesse <1132336+jamolnng@users.noreply.github.com>
2018-10-30 14:35:24 -0500

    added comments

--- JesseLaning_InClassQM.py
+++ JesseLaning_InClassQM.py
@@ -9,23 +9,29 @@ x = np.linspace(-0.5, 0.5, n_steps)
 dx = x[1] - x[0]
 V = np.zeros(n_steps)
 
+# potential for an infinite well
 A = np.zeros((n_steps, n_steps))
 
+# potential for an infinite well with a wall in the middle
 # for i in range(n_steps):
 #     if i > 449 and i < 550:
 #         V[i] = 50000
 
+# potential for a simple harmonic oscillator with a wall in the middle
 # for i in range(n_steps):
 #     V[i] = (i - n_steps/2)**2 * dx * 0.5
 #     if i > 449 and i < 550:
 #         V[i] = 100_000
 
-for i in range(n_steps):
-    if i > 500:
-        V[i] = 50
+# potential for an infinite well with a finite well after 500 steps
+# for i in range(n_steps):
+#     if i > 500:
+#         V[i] = 50
 
+# compute our 1/(dx*dx) so we don't have to keep calculating it and we can just use this variable
 dx2_inv = 1/(dx**2)
 
+# calculate our matrix
 for i in range(n_steps):
     for j in range(n_steps):
         if i == j:
@@ -33,7 +39,9 @@ for i in range(n_steps):
         if i == j - 1 or i == j + 1:
             A[i][j] = -dx2_inv * 0.5
 
+# compute eigen values and eigen vectors
 E, psi_t = np.linalg.eigh(A)
+# take the transpose of psi_t to the wavefunction vectors can accessed as psi[n]
 psi = np.transpose(psi_t)
 
 for i in range(5):
